fcb8ab4f47e334568e69939d07d21dde6246c067
Abubokkor98 <mail.abubokkor@gmail.com>
2025-07-04 01:46:47 +0600

    feat: add AddTaskModal component and integrate with task page; update package dependencies
    -22-11 Create modal using shadcn

--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -1,3 +1,4 @@
+import { AddTaskModal } from "@/components/module/tasks/AddTaskModal";
 import TaskCard from "@/components/module/tasks/TaskCard";
 import { selectFilter, selectTasks } from "@/redux/features/task/taskSlice";
 import { useAppSelector } from "@/redux/hook";
@@ -9,8 +10,9 @@ export default function Task() {
   console.log(filter);
   return (
     <div className="max-w-7xl mx-auto px-5 mt-20">
-      <div>
+      <div className="flex justify-between items-center">
         <h1>Tasks</h1>
+        <AddTaskModal/>
       </div>
       <div className="space-y-5 mt-5">
         {
