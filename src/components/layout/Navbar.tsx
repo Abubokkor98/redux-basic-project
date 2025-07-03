a88a933631e3fe158f7ba01d939e787b98a8296f
Abubokkor98 <mail.abubokkor@gmail.com>
2025-07-03 20:04:28 +0600

    feat: add theme toggle functionality with dropdown menu
    - Added @radix-ui/react-dropdown-menu dependency for dropdown functionality.
    - Implemented ModeToggle component to allow users to switch between light, dark, and system themes.
    - Integrated ModeToggle into Navbar for easy access.
    - Created ThemeProvider to manage theme state and persist user preference in local storage.
    - Updated main entry point to wrap the application in ThemeProvider.
    - Developed custom dropdown menu components for better UI consistency.

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router";
+import { ModeToggle } from "../mode-toggle";
 
 export default function Navbar() {
   return (
-    <nav className="max-w-7xl mx-auto h-16 flex items-center gap-3 px-5">
+    <nav className="max-w-7xl mx-auto h-16 flex items-center justify-between gap-3 px-5">
       <div className="felx items-center">
         <Link to={"/"}>
           {" "}
@@ -11,6 +12,9 @@ export default function Navbar() {
       </div>
       <Link to={"tasks"}>Tasks</Link>
       <Link to={"users"}>Users</Link>
+      <div className="ml-auto">
+        <ModeToggle />
+      </div>
     </nav>
   );
 }
