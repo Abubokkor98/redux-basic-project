a9e33fb46732f39120be8f5d1c4ea477baaf82aa
Abubokkor98 <mail.abubokkor@gmail.com>
2025-07-05 10:32:47 +0600

    23-10 Connecting the baseApi
    -Refactor task management by integrating RTK Query for task retrieval and updating TaskCard component to utilize fetched data

--- src/components/module/tasks/TaskCard.tsx
+++ src/components/module/tasks/TaskCard.tsx
@@ -1,11 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
 import { cn } from "@/lib/utils";
-import {
-  deleteTask,
-  toggleCompleteState,
-} from "@/redux/features/task/taskSlice";
-import { selectUsers } from "@/redux/features/users/userSlice";
+
 import { useAppDispatch, useAppSelector } from "@/redux/hook";
 import type { ITask } from "@/types/task-types";
 
@@ -18,9 +14,9 @@ interface Iprops {
 export default function TaskCard({ task }: Iprops) {
   const dispatch = useAppDispatch();
 
-  const users = useAppSelector(selectUsers);
+  // const users = useAppSelector(selectUsers);
 
-  const assignedUser = users.find((user) => user.id === task.assignedTo);
+  // const assignedUser = users.find((user) => user.id === task.assignedTo);
 
   return (
     <div className="border px-5 py-3 rounded-md">
@@ -39,25 +35,25 @@ export default function TaskCard({ task }: Iprops) {
         </div>
         <div className="flex gap-3 items-center">
           <Button
-            onClick={() => {
-              dispatch(deleteTask(task.id));
-            }}
+            // onClick={() => {
+            //   dispatch(deleteTask(task.id));
+            // }}
             variant="link"
             className="p-0 text-red-500"
           >
             <Trash2 />
           </Button>
           <Checkbox
-            checked={task.isCompleted}
-            onClick={() => {
-              dispatch(toggleCompleteState(task.id));
-            }}
+            // checked={task.isCompleted}
+            // onClick={() => {
+            //   dispatch(toggleCompleteState(task.id));
+            // }}
           />
         </div>
       </div>
       
       <p className="mt-5">{task.description}</p>
-      <p className="mt-2"><span className="font-medium">Assigned To</span> - {assignedUser ? assignedUser.name : "No one"}</p>
+      {/* <p className="mt-2"><span className="font-medium">Assigned To</span> - {assignedUser ? assignedUser.name : "No one"}</p> */}
     </div>
   );
 }
--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -1,12 +1,18 @@
 import { AddTaskModal } from "@/components/module/tasks/AddTaskModal";
+import TaskCard from "@/components/module/tasks/TaskCard";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { useAppDispatch, useAppSelector } from "@/redux/hook";
+import { useGetTaskQuery } from "@/redux/api/baseApi";
+import type { ITask } from "@/types/task-types";
 
 export default function Task() {
-  const dispatch = useAppDispatch();
-  // const filter = useAppSelector(selectFilter);
-  // console.log(tasks);
-  // console.log(filter);
+  const { data, isLoading, isError } = useGetTaskQuery(undefined);
+
+  console.log({ data, isLoading, isError });
+
+  if (isLoading) {
+    return <p>loading...</p>;
+  }
+
   return (
     <div className="max-w-7xl mx-auto px-5 mt-20">
       <div className="flex justify-end items-center gap-5">
@@ -21,7 +27,12 @@ export default function Task() {
         </Tabs>
         <AddTaskModal />
       </div>
-      <div className="space-y-5 mt-5"></div>
+      <div className="space-y-5 mt-5">
+        {!isLoading &&
+          data.tasks.map((task: ITask) => (
+            <TaskCard task={task} key={task.id} />
+          ))}
+      </div>
     </div>
   );
 }
